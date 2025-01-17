407c7b9cf0e5b71423635d0641aa75502566afbd
4lilua <alinesm0503@gmail.com>
2025-01-17 08:47:56 -0300

    continuacao react

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+import { colors } from "../../styles/GlobalStyle"
+
+export const SFooter = styled.footer`
+  /* Rodapé */
+  background-color: ${colors.primary};
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  a{
+    justify-content: space-between;
+    padding-left: 1rem;
+  }
+`
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+
+  justify-content: center;
+  align-items: center;
+  display: flex;
+
+  margin: 0 auto;
+  img {
+    width: 43.25%;
+    justify-content: space-between;
+    padding-left: 5rem;
+  }
+`
